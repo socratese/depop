1cec5f0a71fbc12c7bce4e6c95f214053c0ffe8b
notSocratese <socratese@protonmail.ch>
2021-02-02 23:53:27 +0000

    lol

--- main.js
+++ main.js
@@ -16,6 +16,11 @@ const params = {
     "limit":"3"
 }
 
+const priceFilter = {
+    "min":"25",
+    "max":"170"
+}
+
 const url = `https://webapi.depop.com/api/v1/search/?what=${params.what}&country=${params.country}&offset_id=${params.offset_id}&limit=${params.limit}`
 
 axios.get(url).then(response => {
