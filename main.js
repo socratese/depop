ae207cfa47b44d81a9755aee6206d0e0fef0f2e2
notSocratese <socratese@protonmail.ch>
2021-02-02 22:30:26 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+
+
+
+let account = {
+    "USERNAME":"joesephh",
+    "PASSWORD":"test"
+}
+
+const params = {
+    "what":"nike offwhite",
+    "country":"gb",
+    "offset_id":"MnwyNHwxNjEyMjY4NjU1",
+    "limit":"3"
+}
+
+const url = `https://webapi.depop.com/api/v1/search/?what=${params.what}&country=${params.country}&offset_id=${params.offset_id}&limit=${params.limit}`
+
+axios.get(url).then(response => {
+    response.data.products.forEach(function(item){
+        console.log(item.id)
+    })
+})
